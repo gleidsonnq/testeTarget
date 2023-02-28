19bf02c1c5ea486d2469b99b54c7a2016c630f70
Gleidson <gleidsonnq@gmail.com>
2023-02-28 12:02:44 -0300

    q3Pronta

--- Questao2/q2.py
+++ Questao2/q2.py
@@ -7,14 +7,18 @@ fibo_atual = 1
 
 # verificar se o número informado pertence à sequência
 while fibo_atual < num:
+    #imprimi os dois primeiros valores
+    if(fibo_ant==0):
+        print(f"O sequencia gerada: {fibo_ant} {fibo_atual}",end=" ")
     # calcula o próximo valor da sequência
     fibo_prox = fibo_ant + fibo_atual
     # atualiza os valores dos antecessores
     fibo_ant = fibo_atual
     fibo_atual = fibo_prox
+    print(f"{fibo_atual}",end=" ")
 
-# verifica se o número informado pertence à sequência
+# verificar se o número informado pertence à sequência
 if fibo_atual == num:
-    print(f"O número {num} pertence à sequência de Fibonacci!")
+    print(f"\nO número {num} pertence à sequência de Fibonacci!")
 else:
-    print(f"O número {num} não pertence à sequência de Fibonacci!")
+    print(f"\nO número {num} não pertence à sequência de Fibonacci!")
